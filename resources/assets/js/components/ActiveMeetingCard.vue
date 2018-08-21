c60428e1d5e028ad38aceb8fc698576e8ee4d0c5
s094392 <s094392@gmail.com>
2018-08-22 00:04:34 +0800

    init dev

--- resources/assets/js/components/ActiveMeetingCard.vue
+++ resources/assets/js/components/ActiveMeetingCard.vue
@@ -2,15 +2,15 @@
 <div>
   <div class="uk-card uk-card-default uk-card-body uk-margin-top">
     <div>
-      <router-link :to="{name:'detail', params: {id: 1, view: 'data'}}" class="uk-card-title">WWW 新生訓練</router-link>
+      <router-link :to="{name:'detail', params: {id: meeting.id, view: 'data'}}" class="uk-card-title">{{ meeting.title }}</router-link>
       <span class="uk-label uk-align-right">Badge</span>
     </div>
     <p>
-      <span class="uk-margin-small-right" uk-icon="user" />blzhuang<br>
-      <span class="uk-margin-small-right" uk-icon="tag" />www<br>
-      <span class="uk-margin-small-right" uk-icon="clock" />2018-08-17 13:35
+      <span class="uk-margin-small-right" uk-icon="user" />fddf{{ meeting.owner }}<br>
+      <span class="uk-margin-small-right" uk-icon="tag" />{{ meeting.team }}<br>
+      <span class="uk-margin-small-right" uk-icon="clock" />{{ meeting.scheduled_time }}
     </p>
-    <p>他媽的新生給我來喔<br></p>
+    <p>{{ meeting.description }}<br></p>
     <MeetingControl />
   </div>
 </div>
@@ -21,6 +21,7 @@
 import MeetingControl from "./MeetingControl";
 
 export default {
+  props: ['meeting'],
   components: {
     MeetingControl
   }
--- resources/assets/js/components/ActiveMeetings.vue
+++ resources/assets/js/components/ActiveMeetings.vue
@@ -4,7 +4,7 @@
       <h2 class="uk-display-inline">近期會議</h2>
       <router-link :to="{name:'new'}" class="uk-button uk-button-primary uk-align-right">新增會議</router-link>
     </div>
-    <ActiveMeetingCard v-for="n in 4" :key="n"/>
+    <ActiveMeetingCard v-for="meeting in meetings" :key="meeting.id" :meeting="meeting"/>
   </div>
 </template>
 
@@ -12,8 +12,25 @@
 import ActiveMeetingCard from "./ActiveMeetingCard";
 
 export default {
+  created() {
+    this.fetchMeetings();
+  },
+  data(){
+    return {
+      meetings: []
+    };
+  },
   components: {
     ActiveMeetingCard
+  },
+  methods: {
+    fetchMeetings: function() {
+      axios
+        .get("/api/meeting")
+        .then(response => {
+          this.meetings = response.data;
+        });
+    },
   }
 };
 </script>
